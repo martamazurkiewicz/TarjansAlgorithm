8290da937ed9a57749958c0d86277ff42736b431
martamazurkiewicz <marta@mazurkiewi.cz>
2020-05-05 11:48:50 +0200

    Added list for storing bridges

--- Project/TarjansAlgorithm.cs
+++ Project/TarjansAlgorithm.cs
@@ -6,5 +6,8 @@ namespace Project
 {
     public partial class Graph
     {
+        //Tarjan's algorithm output - list of bridges
+        //integer arrays in list always have 2 elements - 2 vertices of an edge
+        public List<int[]> Bridges { get; private set; }
     }
 }
