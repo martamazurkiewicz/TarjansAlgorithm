68b48a2147c5ccf31f068072d89e91f04b5321a7
Cezary Kurcewicz <czaru66@gmail.com>
2020-05-12 14:30:33 +0200

    refactored code

--- Project/TarjansAlgorithm.cs
+++ Project/TarjansAlgorithm.cs
@@ -8,40 +8,41 @@ namespace Project
     {
         //Tarjan's algorithm output - list of bridges
         //integer arrays in list always have 2 elements - 2 vertices of an edge
+        //used in main window
         public List<int[]> Bridges { get; private set; }
 
-        public int iterator_one;
-        public List<List<int>> graph;
+        public int iterator_one = 1;
+       
         public int[] dfs_numbers;
         public int?[] parents_array;
         public List<int>[] spanning_tree_2;
         public int[] low_array; //array for low parameter for every vertex
         public bool[] visited;
 
-        public Graph(List<List<int>> graph)
+        public void Tarjan_Bridges()
         {
-            this.graph = graph;
             Bridges = new List<int[]>();
-            visited = new bool[graph.Count];
-            low_array = new int[graph.Count];
-            spanning_tree_2 = new List<int>[graph.Count];
-            dfs_numbers = new int[graph.Count];
-            set_Spanning_Tree();
-            set_DFS_Array();
-            set_Parents_Array();
+            visited = new bool[AdjacencyList.Count];
+            low_array = new int[AdjacencyList.Count];
+            spanning_tree_2 = new List<int>[AdjacencyList.Count];
+            dfs_numbers = new int[AdjacencyList.Count];
+            Set_Spanning_Tree();
+            Set_DFS_Array();
+            Set_Parents_Array();
+            Tarjan_Bridges(0);
         }
-        public void Tarjan_Bridges(List<List<int>> Adjacency_List, int First_Vertex, bool[] visited)
+    public void Tarjan_Bridges(int First_Vertex)
         {
 
             int current_Vertex = First_Vertex;
             visited[current_Vertex] = true;
             //   Console.Write(current_Vertex + " ");
 
-            for (int i = 0; i < Adjacency_List[current_Vertex].Count; i++)
+            for (int i = 0; i < AdjacencyList[current_Vertex].Count; i++)
             {
-                if (visited[Adjacency_List[current_Vertex][i]] != true) //unvisited neighboor
+                if (visited[AdjacencyList[current_Vertex][i]] != true) //unvisited neighboor
                 {
-                    Tarjan_Bridges(Adjacency_List, Adjacency_List[current_Vertex][i], visited);
+                    Tarjan_Bridges(AdjacencyList[current_Vertex][i]);
 
                 }
             } //end of branch
@@ -57,19 +58,19 @@ namespace Project
             }
             //checking for other 'current vertex' neighbors
 
-            for (int i = 0; i < graph[current_Vertex].Count; i++)
+            for (int i = 0; i < AdjacencyList[current_Vertex].Count; i++)
             {
                 bool returning_edge = true;
-                if (graph[current_Vertex][i] == parents_array[current_Vertex]) //father
+                if (AdjacencyList[current_Vertex][i] == parents_array[current_Vertex]) //father
                 {
                     returning_edge = false;
                 }
-                if (graph[current_Vertex][i] != parents_array[current_Vertex]) //not father
+                if (AdjacencyList[current_Vertex][i] != parents_array[current_Vertex]) //not father
                 {
 
                     for (int j = 0; j < spanning_tree_2[current_Vertex].Count; j++) //but son
                     {
-                        if (graph[current_Vertex][i] == spanning_tree_2[current_Vertex][j])
+                        if (AdjacencyList[current_Vertex][i] == spanning_tree_2[current_Vertex][j])
                         {
                             returning_edge = false;
                         }
@@ -79,9 +80,9 @@ namespace Project
                 }//not father nor son
                 if (returning_edge == true)
                 {
-                    if (dfs_numbers[graph[current_Vertex][i]] < low_tmp)
+                    if (dfs_numbers[AdjacencyList[current_Vertex][i]] < low_tmp)
                     {
-                        low_tmp = dfs_numbers[graph[current_Vertex][i]];
+                        low_tmp = dfs_numbers[AdjacencyList[current_Vertex][i]];
                     }
                 }
 
@@ -95,21 +96,21 @@ namespace Project
 
 
         }
-        public void set_Spanning_Tree() //dfs spanning tree
+        public void Set_Spanning_Tree() //dfs spanning tree
         {
             for (int i = 0; i < spanning_tree_2.Length; i++)
             {
                 spanning_tree_2[i] = new List<int>();
             }
-            DFS_Spanning_Tree_R(graph, 0, visited, spanning_tree_2);
+            DFS_Spanning_Tree_R(AdjacencyList, 0, visited, spanning_tree_2);
             Clearing_Bool_Table(visited);
         }
-        public void set_DFS_Array() //array for dfs numbers of vertices
+        public void Set_DFS_Array() //array for dfs numbers of vertices
         {
-            DFS_Array_R(graph, 0, visited, dfs_numbers);
+            DFS_Array_R(AdjacencyList, 0, visited, dfs_numbers);
             Clearing_Bool_Table(visited);
         }
-        public void set_Parents_Array() //array of parents for every vertex
+        public void Set_Parents_Array() //array of parents for every vertex
         {
             parents_array = DFS_Parents_Array(spanning_tree_2);
             Clearing_Bool_Table(visited);
@@ -161,15 +162,15 @@ namespace Project
 
         public static int?[] DFS_Parents_Array(List<int>[] DFS_Spanning_Tree) //array for vertices' parents
         {
-            int?[] Parents_Array = new int?[DFS_Spanning_Tree.Length];
+            int?[] parents_array = new int?[DFS_Spanning_Tree.Length];
             for (int i = 0; i < DFS_Spanning_Tree.Length; i++) //parents
             {
                 for (int j = 0; j < DFS_Spanning_Tree[i].Count; j++) //kids
                 {
-                    Parents_Array[DFS_Spanning_Tree[i][j]] = i;
+                    parents_array[DFS_Spanning_Tree[i][j]] = i;
                 }
             }
-            return Parents_Array;
+            return parents_array;
         }
 
     }
