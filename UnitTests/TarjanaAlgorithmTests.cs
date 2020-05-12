1f027383a9d09a9f096e8763d0c720e796adad9a
martamazurkiewicz <marta@mazurkiewi.cz>
2020-05-12 09:40:08 +0200

    Added new test to check if algorithm returns right bridges

--- UnitTests/TarjanaAlgorithmTests.cs
+++ UnitTests/TarjanaAlgorithmTests.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using NUnit.Framework;
+using Project;
 
 namespace UnitTests
 {
+    [TestFixture]
     class TarjanaAlgorithmTests
     {
+        Graph graph;
+        [SetUp]
+        public void SetUp()
+        {
+            graph = new Graph(7);
+            List<string> adjacencyList = new List<string>()
+            {
+                "2,3,4",
+                "1,5,6",
+                "1,4,7",
+                "1,3,7",
+                "2,6",
+                "2,5",
+                "3,4"
+            };
+            graph.AddNeighbors(adjacencyList);
+        }
+        [Test]
+        public void GetBridgesTest()
+        {
+            graph.Tarjan_Bridges(graph.AdjacencyList, 0, new bool[graph.AdjacencyList.Count]);
+            List<int[]> brigdes = new List<int[]>() { new int[] { 0, 1 } };
+            Assert.AreEqual(brigdes, graph.Bridges);
+        }
     }
 }
